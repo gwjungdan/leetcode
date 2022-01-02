28b8c9284f1a8105bb1790d1d6bf67b1e60adef6
gwjungdan <96934585+gwjungdan@users.noreply.github.com>
2022-01-02 21:50:08 +0900

    Create JumpGame.java

---
+++ JumpGame.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        boolean [] d = new boolean[nums.length+1];
+        
+        d[nums.length-1] = true;
+        
+        for(int i=nums.length-2; i>=0; i--){
+            d[i] = false;
+            for(int j=0; j<=nums[i]; j++){
+                if(i + j <= nums.length-1 && d[i+j] == true){
+                    d[i] = true;
+                    break;
+                }
+            }
+            
+        }
+        return d[0];
+    }
+}
